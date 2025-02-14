ec7c052aafd4639514e277a4f44c9bdf8c97644c
Darda <r.dayok@native.semicolon.africa>
2025-02-14 23:09:36 +0100

    implemented the discover section of my portfolio website

--- scripts/header.js
+++ scripts/header.js
@@ -3,8 +3,10 @@ const header = document.querySelector('header')
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
         header.style.background = "rgba(36, 38, 42, 1)"; 
+        header.style.height = "55px";
     } else {
         header.style.background = "rgba(0, 0, 0, 0)";
+        header.style.height = "100px"
     }
 });
 
